db201cc4eba712652e714517e3c84762f7aafa0e
Nimit-sethi <76746642+Nimit-sethi@users.noreply.github.com>
2023-06-19 20:10:56 +0530

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,5 +1,5 @@
 class Secrets {
   // Add your Google Maps API Key here
-  static const apikey = 'AIzaSyBZoO2h4LXBdTFli5zjsnX5jkBksZc9qF8';
+  static const apikey = '';
   // static const apikey = 'AIzaSyDKiF8rXkbHfSR4IptF7OhUu5Zh4eosz64';
 }
